82124751194c8084858c26df8fee46e59900b5fc
xQiwix <159818062+xQiwix@users.noreply.github.com>
2024-06-04 16:26:04 +0300

    B176.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,36 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> Tree;
+  std::ifstream file(filename);
+  std::string strfl = "";
+  if (!file) {
+    std::cout << "Error!" << std::endl;
+    return Tree;
+  }
+  while (!file.eof()) {
+    char x = file.get();
+    strfl += x;
+  }
+  for (char& n : strfl) {
+    n = std::tolower(n);
+  }
+  int length = strfl.length();
+  bool wrd = false;
+  std::string tmp = "";
+  for (int i = 0; i < length; i++) {
+    if (strfl[i] >= 'a' && strfl[i] <= 'z') {
+      wrd = true;
+    } else {
+      wrd = false;
+    }
+    if (wrd) {
+      tmp += strfl[i];
+    } else {
+      Tree.add(tmp);
+      tmp = "";
+    }
+  }
+  file.close();
+  return Tree;
 }
